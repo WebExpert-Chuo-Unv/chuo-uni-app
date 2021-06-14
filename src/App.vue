0d5a5d5d50472431b0800e5c4a46175528347d1b
koutarou <kou06yokohama@gmail.com>
2021-06-14 11:39:31 +0900

    投稿フォーム

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,12 @@
 <template>
-  <PostForm />
+  <div>
+    <div>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> |
+      <router-link to="/my-page">MyPage</router-link>
+    </div>
+    <div><PostForm /></div>
+  </div>
 </template>
 
 <script>
